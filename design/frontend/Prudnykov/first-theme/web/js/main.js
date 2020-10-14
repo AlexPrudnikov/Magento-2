51988dc6cc9e2965deef7f7e45e504b1a2c095b1
Shnur <Ra.Residentadvisor@gmail.com>
2020-10-14 22:38:38 +0300

    first-theme

---
+++ design/frontend/Prudnykov/first-theme/web/js/main.js
@@ -0,0 +1,38 @@
+require(['jquery', 'slick'], function($){
+	$(function(){
+		$('.slider-product-items').slick({
+		  infinite: true,
+		  slidesToShow: 4,
+		  slidesToScroll: 1,
+		  adaptiveHeight: true,
+		  responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 3,
+		        infinite: true,
+		        dots: false
+		      }
+		    },
+		    {
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		    // You can unslick at a given breakpoint now by adding:
+		    // settings: "unslick"
+		    // instead of a settings object
+		  ]
+		});
+	});
+});
